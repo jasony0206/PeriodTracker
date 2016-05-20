b55ac059eedd65ee4e3807f2aa8cf5b0db21806a
Jason Yoon <jasony0206@gmail.com>
2016-05-19 17:16:16 -0700

    Compute expected period date

--- period.rb
+++ period.rb
@@ -2,13 +2,17 @@
 # 	input periods so far: start date, end date
 # 	tells you when your next period is expected to be
 
+# TODO: 
+# 	persistent storage of data
+# 	what happens if you forget to report a period?
+
 require 'date'
 
 # prompt user for period start dates and store
 puts "When were the first days of your menstruation period? (MM/DD/YYYY)"
 first_days = [];
 while !(day = gets.chomp).empty?
-	date = DateTime.strptime(day, '%m/%d/%Y')
+	date = Date.strptime(day, '%m/%d/%Y')
 	first_days << date
 end
 
@@ -19,4 +23,11 @@ cycle_lengths = first_days.each_cons(2).map do |day1, day2|
 end
 
 avg_cycle = cycle_lengths.reduce(:+) / cycle_lengths.length
-puts avg_cycle
+puts "Average cycle length: #{avg_cycle}"
+
+puts "When was the first day of your last period?"
+last_period_day = gets.chomp
+last_period = Date.strptime(last_period_day, '%m/%d/%Y')
+expected_period = last_period + avg_cycle
+
+puts "Your next period is expected to be on #{expected_period.strftime('%m/%d/%Y')}"
